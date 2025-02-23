c51cff42b9e2e667edaaa4588d584b0460606524
adrian-tompkins <adrian-tompkins>
2025-02-23 08:02:51 +0000

    Descriptions

--- Lab 01 - DI Platform/2.2 Data Prep - Gold.sql
+++ Lab 01 - DI Platform/2.2 Data Prep - Gold.sql
@@ -2,11 +2,11 @@
 -- MAGIC %md
 -- MAGIC ## Create a Gold Table
 -- MAGIC
--- MAGIC Have a go at creating a gold table, using the databricks assistant to help. You may want to try something like asking the assistant to change the grain of the silver_hme_event table that was created, summing over the duration.
+-- MAGIC Have a go at creating a gold table, using the databricks assistant to help. You may want to try something like asking the assistant to change the grain of the silver_hme_event table that was created, summing over the duration, grouping by site, etc.
 -- MAGIC
 -- MAGIC Once you are happy, write the table out.
 -- MAGIC
--- MAGIC Feel free to add other gold tables if desired.
+-- MAGIC Feel free to add more gold tables if desired.
 -- MAGIC
 
 -- COMMAND ----------
